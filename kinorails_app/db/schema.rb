18fc3d425137ac722f048e419ca8069d93cc4e92
illumitata <gothic8282@gmail.com>
2018-11-29 23:39:22 +0100

    Added columns and roles to Users table and model

--- kinorails_app/db/schema.rb
+++ kinorails_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_29_222501) do
+ActiveRecord::Schema.define(version: 2018_11_29_223702) do
 
   create_table "rooms", force: :cascade do |t|
     t.integer "seats"
@@ -27,6 +27,11 @@ ActiveRecord::Schema.define(version: 2018_11_29_222501) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "login", default: "", null: false
+    t.string "name", default: "", null: false
+    t.string "surname", default: "", null: false
+    t.string "phone_number", default: "", null: false
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
