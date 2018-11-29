479ddfdae374f191e2e62063aa5c773e76be55f9
illumitata <gothic8282@gmail.com>
2018-11-29 23:30:57 +0100

    Added working sing_up and login pages

--- kinorails_app/app/controllers/application_controller.rb
+++ kinorails_app/app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+    # Uncomment if you want user to login every time
+    # he enters any of pages
+    # before_action :authenticate_user!
 end
