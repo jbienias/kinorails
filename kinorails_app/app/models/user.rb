7a745f55b6c91a2977dc4a0286b9f571a3cba44f
illumitata <gothic8282@gmail.com>
2018-12-04 22:44:56 +0100

    Dodanie walidacji modeli

--- kinorails_app/app/models/user.rb
+++ kinorails_app/app/models/user.rb
@@ -1,8 +1,30 @@
 class User < ApplicationRecord
   has_many :favourite_movies
   has_many :reservations
+
+  validates_uniqueness_of :username
+
   enum role: [:user, :admin]
   after_initialize :set_default_role, :if => :new_record?
+
+  validates :username, \
+  :length => { :in => 1..30 }, \
+  allow_empty: false
+
+  validates :name, \
+  :length => { :in => 1..30 }, \
+  allow_empty: false
+
+  validates :surname, \
+  :length => { :in => 1..30 }, \
+  allow_empty: false
+
+  validates :phone_number, \
+  allow_empty: false, \
+  format: { :with => /\A(\d{2}[ ])?\d{3}[ ]\d{2,3}[ ]\d{2,3}\z/, \
+    :message => " zły format numeru telefonu. (xx xxx xx[x] xx[x])" }
+  # Czy to konieczne?
+  # uniques: true
 
   def set_default_role
     self.role ||= :user
