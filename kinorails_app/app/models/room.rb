3142f8be2aaf223debfecb4fdddbb5c2a77b68b7
illumitata <gothic8282@gmail.com>
2018-12-10 11:40:08 +0100

    Dodanie walidacji modeli (#15)

--- kinorails_app/app/models/room.rb
+++ kinorails_app/app/models/room.rb
@@ -1,3 +1,10 @@
 class Room < ApplicationRecord
   has_many :seats
+
+  validates_uniqueness_of :name
+
+  validates :name, \
+  :length => { :in => 1..50 }, \
+  allow_blank: false, \
+  allow_empty: false
 end
--- kinorails_app/app/models/screening.rb
+++ kinorails_app/app/models/screening.rb
@@ -2,4 +2,20 @@ class Screening < ApplicationRecord
   belongs_to :movie
   belongs_to :room
   has_many :reservations
+
+  validates :movie_id, \
+  numericality: { greater_than_or_equal_to: 0 }
+
+  validates :room_id, \
+  numericality: { greater_than_or_equal_to: 0 }
+
+  validate :date_cannot_be_in_the_past
+
+  # ewentualne poprawki konieczne
+  def date_cannot_be_in_the_past
+    if date.present? && date < Time.now # Date.today
+      errors.add(:date, "nie może być w przeszłości!")
+    end
+  end
+
 end
