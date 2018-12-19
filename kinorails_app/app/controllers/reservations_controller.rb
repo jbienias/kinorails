3041c5519fdecb7c385261c7c82b78ccf3186447
illumitata <gothic8282@gmail.com>
2018-12-19 21:38:55 +0100

    Dodanie wyświetlania sali

--- kinorails_app/app/controllers/reservations_controller.rb
+++ kinorails_app/app/controllers/reservations_controller.rb
@@ -11,6 +11,34 @@ class ReservationsController < ApplicationController
   def new
     @reservation = Reservation.new
     @screening = Screening.find(params[:current_screening_id])
+    @seats = (Seat.all.where(:room_id => @screening.room_id)).to_a
+    @plan = convert_to_a
+
+    @reserved_seats = []
+
+    @seats.each do |s|
+      res_tmp = ReservedSeat.where(:seat_id => s.id).first # ew. find
+      if res_tmp
+        @reserved_seats << res_tmp
+      end
+    end
+  end
+
+  def convert_to_a
+    max_x = @seats[-1].pos_x
+    max_y = @seats.max_by(&:pos_y).pos_y
+
+    @plan = Array.new(max_y + 1) { Array.new(max_x + 1) }
+
+    @seats.each do |s|
+      @plan[s.pos_y][s.pos_x] = s.type_of_seat # =
+    end
+
+    @plan.length.times do |i|
+      @plan[i] = @plan[i].map {|e| e.nil? ? 0 : e}
+    end
+
+    @plan
   end
 
   def create
