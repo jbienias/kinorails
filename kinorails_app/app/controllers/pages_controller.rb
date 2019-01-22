9599ac575e0e7d3ca2675d4c23873ec552f6f1de
M. Barzowska <barzowskam@gmail.com>
2019-01-22 03:25:33 +0100

    Hotfixy (#21)
    * Fav movie jest linkiem do movie
    
    * Ostylowanie formularzy na mniej przestrzenne boxy
    
    * Dostylowanie formularzy, dodanie favikonki
    
    * Bordery na zle wypelnione formy
    
    * Dostylowanie
    
    * Dostylowanie
    
    * Poprawa komunikatow wyszukiwarki
    
    * Dostylowanie
    
    * Jumbotron na hello page
    
    * Jumbotron na filmy
    
    * Jumbotron na filmy

--- kinorails_app/app/controllers/pages_controller.rb
+++ kinorails_app/app/controllers/pages_controller.rb
@@ -1,6 +1,19 @@
 class PagesController < ApplicationController
   #todo
+  
   def show
+    @movies_selected = Movie.all
+    if @movies_selected.length > 3
+      rand_list = (0...@movies_selected.length).to_a.shuffle[0..2]
+
+      movies_tmp = []
+
+      rand_list.each do |num|
+        movies_tmp << @movies_selected[num]
+      end
+
+      @movies_selected = movies_tmp
+    end
     render :file => "/pages/_help.html.erb"
   end
 
